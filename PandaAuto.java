a92f6e986c7f7bc99851f4e33f85d416345abb1a
Michael Ching <michaeljune101@yahoo.com>
2022-04-22 12:27:21 -0700

    Update PandaAuto.java

--- PandaAuto.java
+++ PandaAuto.java
@@ -8,6 +8,7 @@
  */
 
 import java.time.Duration;
+import java.util.Scanner;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.safari.*;
@@ -22,18 +23,38 @@ public class PandaAuto
 	public static void main(String[] args)
 	{
 	
+		Scanner scan = new Scanner(System.in);
 		
-		//WebDriver driver = new SafariDriver(); 
-		SafariOptions options = new SafariOptions();
-		WebDriver  driver = new SafariDriver(options);
+		System.out.println("Enter your panda express code:");
+		String code = scan.nextLine();
+		
+		//Check code is proper length
+		while(code.length() != 22)
+		{
+			
+			System.out.println("Code is not proper length");
+			code = scan.nextLine();
+			
+		}
+		
+		
+		WebDriver driver = new SafariDriver(); 
+		//SafariOptions options = new SafariOptions();
+		//WebDriver  driver = new SafariDriver(options);
 		  
 	
 		driver.get("https://www.pandaguestexperience.com/Index.aspx?POSType=SmartCode");
 		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
-		driver.findElement(By.name("CN1")).sendKeys("test");
-		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+		driver.findElement(By.name("CN1")).sendKeys(code.substring(0, 4));
+		driver.findElement(By.name("CN2")).sendKeys(code.substring(4, 8));
+		driver.findElement(By.name("CN3")).sendKeys(code.substring(8, 12));
+		driver.findElement(By.name("CN4")).sendKeys(code.substring(12, 16));
+		driver.findElement(By.name("CN5")).sendKeys(code.substring(16, 20));
+		driver.findElement(By.name("CN6")).sendKeys(code.substring(20, 22));
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+		driver.findElement(By.id("NextButton")).click();
 		
-		driver.quit();
+		//driver.quit();
 		
 		
 	}
