9c883f577568f87b89f22871ebd43a1b162d36d8
Michael Ching <michaeljune101@yahoo.com>
2022-04-21 20:45:50 -0700

    Initial commit

---
+++ PandaAuto.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Michael Ching
+ *
+ */
+public class PandaAuto
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
